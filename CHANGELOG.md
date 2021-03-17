694e5202dab48a79d9d8c4860beb0a5e7c82c2d5
mathru <user@mathru.net>
2021-03-17 23:02:41 +0900

    コメントの追加など

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [0.8.1] - Update README (2021.03.11)
+
+* Main release to coincide with Flutter 2.
+* Add a comment.
+
 ## [0.7.0] - Update README (2021.02.05)
 
 * The first release.
--- lib/firebase/firebase_core.dart
+++ lib/firebase/firebase_core.dart
@@ -1,15 +1,27 @@
 part of katana_firebase;
 
+/// Class that prepares to handle Firebase.
+///
+/// Please be sure to initialize by executing [initialize].
+///
+/// ```
+/// await FirebaseCore.initialize();
+/// ```
 class FirebaseCore {
   FirebaseCore._();
   static FirebaseApp? _app;
 
   /// True if initialization has been completed.
   static bool get isInitialized => _app != null;
-  static Future initialize() async {
+
+  static late String region;
+
+  /// Initialize Firebase.
+  static Future<void> initialize({String region = "asia-northeast1"}) async {
     if (_app != null) {
       return;
     }
+    FirebaseCore.region = region;
     await Localize.initialize(locale: "en_US");
     _app = await Firebase.initializeApp();
     FirebaseFirestore.instance.settings = const Settings();
